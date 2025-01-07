78f6ee2e9994de2520cb434e7cdddccb84054a05
Giovanni Bassi <giggio@giggio.net>
2025-01-07 16:22:13 -0300

    Install without ssh key

--- remote_install.sh
+++ remote_install.sh
@@ -8,10 +8,19 @@ if [[ `id -u` == '0' ]]; then
   echo "Don't run this as root"
   exit 1
 fi
+if readlink $HOME/.config &> /dev/null; then
+  echo "$HOME/.config is already symlinked"
+else
+  ln -s /data/giggio/.config/ $HOME/.config
+fi
 if [ -d $HOME/.dotfiles ]; then
   echo "You already have a $HOME/.dotfiles directory"
   exit 1
 else
-  git clone --recurse-submodules git@github.com:giggio/dotfiles-homeassistant.git $HOME/.dotfiles
+  # Using https instead of ssh because we are forwarding the ssh key and this is run by the Home Assistant add-on
+  git clone --recurse-submodules https://github.com/giggio/dotfiles-homeassistant.git $HOME/.dotfiles
   $HOME/.dotfiles/install
+  cd $HOME/.dotfiles
+  git remote set-url origin git@github.com:giggio/dotfiles-homeassistant.git
+  git submodule sync
 fi
